c790c71dca3a4cf93376fbea01592caaa7129a5c
Levi Duarte <duartelevi1@gmail.com>
2024-11-21 22:13:57 -0300

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'bg-section1': "url('./src/assets/bg-1.jpg')",
-        'bg-section2': "url('./src/assets/bg-2.jpg')",
-        'bg-section3': "url('./src/assets/bg-3.jpg')",
+        'bg-section1': "url('./assets/bg-1.jpg')",
+        'bg-section2': "url('./assets/bg-2.jpg')",
+        'bg-section3': "url('./assets/bg-3.jpg')",
       }
     },
   },
